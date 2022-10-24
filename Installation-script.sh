7c15b68bc8785369efb66cb17f9bb7ec39bf8358
aktsu_ruan <77154574+ruanpereira@users.noreply.github.com>
2022-10-24 18:57:55 -0300

    Script de nova instalação,

---
+++ Installation-script.sh
@@ -0,0 +1,36 @@
+
+
+sudo pacman-mirrors --fasttrack && sudo pacman -Syu
+install_pulse
+sudo pacman -S firefox
+sudo pacman -S bitwarden
+sudo pacman -S pycharm-community-edition
+sudo pacman -S telegram-desktop
+sudo pacman -S github-cli
+sudo pacman -S discord
+sudo pacman -S whatsapp-web-jak
+sudo pacman -S wine
+sudo yay -S clion  
+sudo pacman -S okular
+sudo pacman -S code
+sudo yay -S slack
+sudo pacman -S gnome-calculator
+sudo pacman -S git
+sudo pacman -S base-devel
+sudo yay -S ani-cli 
+sudo pacman -S spotify-launcher
+sudo pacman -S python-pip
+sudo pacman -S flameshot
+sudo pacman -S virtualbox
+sudo pacman -S arduino
+sudo pacman -S neofetch
+sudo pacman -S jupyter-notebook
+sudo pacman -S emacs
+neofetch
+
+#script para a instalação de tudo, ainda é necessário fazer as alterações 
+#nos arquivos de configuração salvados aqui: 
+#(~/.profile, ~/.i3/config e ~/.config/mimeapps.list)
+# nos arquivos acima, trocar tudo de palemoon para firefox
+# e o editor padrão de pdf para org.kde.okular.desktop
+#https://wiki.manjaro.org/index.php/Pacman-mirrors
